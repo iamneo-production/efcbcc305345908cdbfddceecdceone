8c5cc04f5afc6ec37ec95e8a929f81e236ed0d8b
Iamneo <techadmin@iamneo.ai>
2023-09-23 06:48:59 +0000

    Backup Commit

--- Ohio.tf
+++ Ohio.tf
@@ -1,7 +1,20 @@
 terraform {
     required_providers {
         aws = {
-            s
+            source = "hashicorp/aws"
+            version ="~> 5.0"
         }
     }
+}
+provider "aws" {
+    region ="us-east-2"
+    access_key ="AKIAZXAGDAPR3IYWNNU4"
+    secret_key ="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"
+}
+resource "aws_instance" "ncos" {
+    ami =""
+    instance_type ="t2.micro"
+}
+output "ncos Public_ip" {
+    value=aws_instance.ncos.public-ip
 }
